4f4c020fbff4a07490a90e107f22b2a18ae6d256
aux-Issa <issa1116184028@gmail.com>
2022-07-24 01:16:26 +0900

    debug

--- app/infrastructure/sqlhandler.go
+++ app/infrastructure/sqlhandler.go
@@ -3,11 +3,17 @@ package infrastructure
 import (
 	"database/sql"
 	// _ "github.com/go-sql-driver/mysql"
-	// "github.com/aux-Issa/Next_Go_practice/app/interface/database"
+	"github.com/aux-Issa/Next_Go_practice/app/interface/database"
 )
 
 type SqlHandler struct {
 	Conn *sql.DB
+}
+type SqlResult struct {
+	Result sql.Result
+}
+type SqlRow struct {
+	Rows *sql.Rows
 }
 
 func NewSqlHandler(conn *sql.DB) *SqlHandler {
@@ -21,7 +27,7 @@ func NewSqlHandler(conn *sql.DB) *SqlHandler {
 }
 
 func (handler *SqlHandler) Execute(statement string, args ...interface{}) (database.Result, error) {
-	res := SqlHandler{}
+	res := new(SqlResult)
 	result, err := handler.Conn.Exec(statement, args...)
 	if err != nil {
 		return res, err
@@ -30,18 +36,15 @@ func (handler *SqlHandler) Execute(statement string, args ...interface{}) (datab
 	return res, nil
 }
 
-func (handler *SqlHandler) Query(statement string, args ...interface{}) database.Row {
+func (handler *SqlHandler) Query(statement string, args ...interface{}) (database.Row, error) {
 	rows, err := handler.Conn.Query(statement, args...)
 	if err != nil {
-		return new(SqlHandler), err
+		return nil, err
 	}
-	row := new(SqlHandler)
+	row := new(SqlRow)
 	row.Rows = rows
-	return row, err
-}
-
-type SqlResult struct {
-	Result sql.Result
+	// ⇩ここrowかもしれない
+	return rows, err
 }
 
 func (r SqlResult) LastInsertId() (int64, error) {
@@ -50,10 +53,6 @@ func (r SqlResult) LastInsertId() (int64, error) {
 
 func (r SqlResult) RowsAffected() (int64, error) {
 	return r.Result.RowsAffected()
-}
-
-type SqlRow struct {
-	Rows *sql.Rows
 }
 
 func (r SqlRow) Scan(...interface{}) error {
