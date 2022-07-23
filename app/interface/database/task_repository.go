9aebc6725b3703b4a81b8fe3e16b20d6d5ac4ff7
aux-Issa <issa1116184028@gmail.com>
2022-07-24 01:23:24 +0900

    typo

--- app/interface/database/task_repository.go
+++ app/interface/database/task_repository.go
@@ -4,9 +4,7 @@ import (
 	"github.com/aux-Issa/Next_Go_practice/app/domain"
 )
 
-import 
-
-type TaskRepository interface {
+type TaskRepository struct {
 	SqlHandler
 }
 
@@ -14,7 +12,7 @@ func (repo *TaskRepository) Store(task domain.Task) (err error) {
 	_, err = repo.Execute(
 		"INSERT INTO tasks (title, content) VALUES(?,?)",
 		task.Title,
-		task.Content
+		task.Content,
 	)
 	if err != nil {
 		return
@@ -22,11 +20,11 @@ func (repo *TaskRepository) Store(task domain.Task) (err error) {
 	return
 }
 
-func (repo *TaskRepository) FindById(id int)(task domain.Task, err error){
-	row, err := repo.Query("SELECT id, title, content FROM tasks WHERE id=?", id)
+func (repo *TaskRepository) FindById(given_id int) (task domain.Task, err error) {
+	row, err := repo.Query("SELECT id, title, content FROM tasks WHERE id=?", given_id)
 	defer row.Close()
 	if err != nil {
-		return		
+		return
 	}
 	var id int
 	var title string
@@ -42,27 +40,29 @@ func (repo *TaskRepository) FindById(id int)(task domain.Task, err error){
 	task.Title = title
 	task.Content = content
 	return
- }
+}
 
-func (repo *TaskRepository) FindAll(tasks domain.Tasks, err error){
+func (repo *TaskRepository) FindAll() (tasks domain.Tasks, err error) {
 	rows, err := repo.Query("SELECT id, title, conetent FORM tasks")
 	defer rows.Close()
-	if err != nil {
-		return
-	}
-	for rows.Next(){
+	// if err != nil {
+	// 	// return err
+	// 	// continue
+	// }
+	for rows.Next() {
 		var id int
 		var title string
 		var content string
 		if err = rows.Scan(&id, &title, &content); err != nil {
-			return
+			return nil, err
+			// continue
 		}
 		task := domain.Task{
-			ID:       id,
-			Title:    title,
-			Content:  content,
+			ID:      id,
+			Title:   title,
+			Content: content,
 		}
 		tasks = append(tasks, task)
 	}
-	return tasks
+	return tasks, nil
 }
